88edb73fadcbb7f19dc7bfd8c3f1cb2bef589250
aikuakdi <1774104802@qq.com>
2017-07-05 14:09:46 +0800

    add table

--- msg_sql/task_job配置脚本.sql
+++ msg_sql/task_job配置脚本.sql
@@ -335,6 +335,25 @@ INSERT INTO config_rely_log(task_name,parent_obj,updatedt)
 VALUES("idl_msg_sender_receiver_relationship_agg",
     "idl_msg_received_join_log",
      now());  
+
+-- idl_msg_receiver_tag_agg
+INSERT INTO config_task_log(task_name,file_path,insertdt,updatedt,connect_type,task_type,outtimes)
+VALUES ("idl_msg_receiver_tag_agg",
+     "./task_file/msg_daily/idl_msg_receiver_tag_agg.sql",
+      now(),
+      now(),
+     "hive_f",
+     "daily",
+     "03:00:00");
+INSERT INTO config_taregt_log(task_name,target_obj,is_chack,updatedt)
+VALUES("idl_msg_receiver_tag_agg",
+    "idl_msg_receiver_tag_agg",
+     0,
+     now());
+INSERT INTO config_rely_log(task_name,parent_obj,updatedt)
+VALUES("idl_msg_receiver_tag_agg",
+    "idl_msg_receiver_info_agg",
+     now());  
      
 -- ≈‰÷√job–≈œ¢
 INSERT INTO plan_job_config (job_name,job_type,PARAMETER,par_end,PARALLEL,begin_time,insertdt,updatedt,job_status,is_debug)
@@ -416,4 +435,8 @@ VALUES("msg_daily",
 INSERT INTO plan_orltask_log(job_name,task_name,insertdt)
 VALUES("msg_daily",
     "idl_msg_sender_receiver_relationship_agg",
-     now());      
+     now()); 
+INSERT INTO plan_orltask_log(job_name,task_name,insertdt)
+VALUES("msg_daily",
+    "idl_msg_receiver_tag_agg",
+     now());  
